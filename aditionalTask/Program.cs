e729e19aae625e4af20eb757976462f8c5c4c913
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-22 20:18:08 +0200

    Добавьте файлы проекта.

---
+++ aditionalTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace aditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userCollection = new UserCollection();
+
+            Console.WriteLine("Random array of int numbers: ");
+            for (int i = 0; i < userCollection.Count; i++)
+                Console.Write(userCollection[i] + " ");
+
+            Console.WriteLine("Random array of int numbers: ");
+            foreach (int el in UserCollection)
+                Console.Write(el + " ");
+        }
+
+        
+    }
+}
---
+++ aditionalTask/UserCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace aditionalTask
+{
+    class UserCollection
+    {
+        private int[] array = new int[10];
+
+        public int Count
+        {
+            get => array.Length;
+        }
+        public int this[int index] 
+        {
+            set { array[index] = value; } 
+            get { return array[index]; } 
+        }
+        public UserCollection()
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(0, 9);
+        }
+        public IEnumerator GetEnumerator(int[] array)
+        {
+            int position = -1;
+            while (true)
+            {
+                if (position < array.Length - 1)
+                {
+                    position++;
+                    if ((array[position] % 2) == 1)
+                        yield return Math.Pow(array[position], 2);
+                }
+                else
+                    yield break;
+            }
+        }
+    }
+}
