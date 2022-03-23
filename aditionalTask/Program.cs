826c081a42778d39b24dace1b436165149bab2d9
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-23 06:49:31 +0200

    Изменил решение дополнительного задания

--- aditionalTask/Program.cs
+++ aditionalTask/Program.cs
@@ -7,17 +7,35 @@ namespace aditionalTask
     {
         static void Main(string[] args)
         {
-            var userCollection = new UserCollection();
+            Random random = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(0, 9);
 
             Console.WriteLine("Random array of int numbers: ");
-            for (int i = 0; i < userCollection.Count; i++)
-                Console.Write(userCollection[i] + " ");
+            foreach (int el in array)
+                Console.Write(el + " ");
 
-            Console.WriteLine("Random array of int numbers: ");
-            foreach (int el in UserCollection)
+            IEnumerable newArray = CreateCollectionOfPowOddNumber(array);
+            Console.WriteLine("\nArray of pow odd numbers: ");
+            foreach (int el in newArray)
                 Console.Write(el + " ");
         }
 
-        
+        static IEnumerable CreateCollectionOfPowOddNumber(int[] array)
+        {
+            int position = -1;
+            while (true)
+            {
+                if (position < array.Length - 1)
+                {
+                    position++;
+                    if ((array[position] % 2) == 1)
+                        yield return (int)Math.Pow(array[position], 2);
+                }
+                else
+                    yield break;
+            }
+        }
     }
 }
